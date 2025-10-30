ca5c3fc4d5f2230609fed90244068263d9b0071d
gichabu <kariuki.26174@students.kyu.ac.ke>
2025-10-30 20:05:28 +0300

    Add files via upload
    Signed-off-by: gichabu <kariuki.26174@students.kyu.ac.ke>

---
+++ revenue_tracker.c
@@ -0,0 +1,24 @@
+/*
+Name:Ian kariuki 
+Reg:CT100/G/26174/25
+Description: revenue tracker
+*/
+#include<stdio.h>
+int main  () {
+int i,total=0;
+
+int revenue [7]={2,3,4,6,8,10,12};
+
+printf ("%d", revenue[0]);
+printf ("%d", revenue [1]);
+printf ("%d", revenue [2]);
+printf ("%d", revenue [3]);
+printf ("%d", revenue [4]);
+printf ("%d", revenue [5]);
+printf ("%d\n", revenue [6]);
+total+=revenue[i];
+
+printf("%d", total);
+
+return 0;
+}
